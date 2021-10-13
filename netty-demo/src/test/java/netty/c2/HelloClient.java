3526374785e60437020e534d0bd7141f19827d9a
TonyHuang <1578501542@qq.com>
2021-10-13 20:00:09 +0800

    第一个 Netty 范例

--- netty-demo/src/test/java/netty/c2/HelloClient.java
+++ netty-demo/src/test/java/netty/c2/HelloClient.java
@@ -1,4 +1,4 @@
-package netty.c1;
+package netty.c2;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelInitializer;
--- netty-demo/src/test/java/netty/c2/HelloServer.java
+++ netty-demo/src/test/java/netty/c2/HelloServer.java
@@ -1,4 +1,4 @@
-package netty.c1;
+package netty.c2;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandlerContext;
