be7f206c4f181f5f7f6ad1ceb7ff5b0aeaeede28
TonyHuang <1578501542@qq.com>
2021-10-20 21:56:48 +0800

    定长编码器解决黏包半包演示

---
+++ netty-demo/src/test/java/advance/HelloWorldClient2.java
@@ -0,0 +1,67 @@
+package advance;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+public class HelloWorldClient2 {
+    static final Logger log = LoggerFactory.getLogger(HelloWorldClient2.class);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            send();
+        }
+        System.out.println("finish");
+    }
+
+    private static void send() {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(worker);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        // 在连接 channel 建立成功后，会触发 active 事件
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            ByteBuf buf = ctx.alloc().buffer();
+                            char c = '0';
+                            Random r = new Random();
+                            for (int i = 0; i < 10; i++) {
+                                byte[] bytes = new byte[10];
+                                for (int j = 0; j < r.nextInt(10); j++) {
+                                    bytes[j] = (byte) c;
+                                }
+                                c++;
+                                buf.writeBytes(bytes);
+                            }
+                            ctx.writeAndFlush(buf);
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("client error", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
---
+++ netty-demo/src/test/java/advance/HelloWorldServer2.java
@@ -0,0 +1,53 @@
+package advance;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.AdaptiveRecvByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloWorldServer2 {
+    void start() {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            // 调整系统的接受缓冲区（滑动窗口）
+            // serverBootstrap.option(ChannelOption.SO_RCVBUF, 10);
+            // 调整 netty 的接受缓冲区（bytebuf）
+            serverBootstrap.childOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(16, 16, 16));
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    // 定长编码器(FixedLengthFrameDecoder)
+                    // 约定消息长度固定为 n
+                    // 遇到半包的内容，等待后续消息到来，长度够 n，再将消息发给下一个 handler
+                    // 遇到长度 > n 的消息，发送前 n 长度的消息，多出来的部分等待后续消息到来拼接
+                    ch.pipeline().addLast(new FixedLengthFrameDecoder(10));
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("server error", e);
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new HelloWorldServer2().start();
+    }
+}
