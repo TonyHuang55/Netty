fbf25a9be72ca8654c8f5d02b9c0b731b1957c44
TonyHuang <1578501542@qq.com>
2021-10-23 18:27:26 +0800

    行解码器解决黏包半包演示

---
+++ netty-demo/src/test/java/advance/c1/HelloWorldClient3.java
@@ -0,0 +1,72 @@
+package advance;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+public class HelloWorldClient3 {
+    static final Logger log = LoggerFactory.getLogger(HelloWorldClient3.class);
+
+    public static void main(String[] args) {
+        send();
+        System.out.println("finish");
+    }
+
+    public static StringBuilder makeString(char c, int len) {
+        StringBuilder sb = new StringBuilder(len + 1);
+        for (int i = 0; i < len; i++) {
+            sb.append(c);
+        }
+        sb.append("\n");
+        return sb;
+    }
+
+    private static void send() {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(worker);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        // 在连接 channel 建立成功后，会触发 active 事件
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            ByteBuf buf = ctx.alloc().buffer();
+                            char c = '0';
+                            Random r = new Random();
+                            for (int i = 0; i < 10; i++) {
+                                StringBuilder sb = makeString(c, r.nextInt(256) + 1);
+                                c++;
+                                buf.writeBytes(sb.toString().getBytes());
+                            }
+                            ctx.writeAndFlush(buf);
+                        }
+                    });
+                }
+            });
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 8080).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("client error", e);
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+}
