a82bd7bd70b198b71df7c67b2b2007215e1017ca
TonyHuang <1578501542@qq.com>
2021-11-01 22:11:59 +0800

    登录功能

--- netty-demo/src/main/java/server/ChatServer.java
+++ netty-demo/src/main/java/server/ChatServer.java
@@ -5,11 +5,14 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
+import message.LoginRequestMessage;
+import message.LoginResponseMessage;
 import protocol.MessageCodecSharable;
+import protocol.ProcotolFrameDecoder;
+import server.service.UserServiceFactory;
 
 @Slf4j
 public class ChatServer {
@@ -25,10 +28,24 @@ public class ChatServer {
             serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new LengthFieldBasedFrameDecoder(
-                            1024, 12, 4, 0, 0));
+                    ch.pipeline().addLast(new ProcotolFrameDecoder());
                     ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast(new SimpleChannelInboundHandler<LoginRequestMessage>() {
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
+                            String username = msg.getUsername();
+                            String password = msg.getPassword();
+                            boolean login = UserServiceFactory.getUserService().login(username, password);
+                            LoginResponseMessage message;
+                            if (login) {
+                                message = new LoginResponseMessage(true, "登录成功");
+                            } else {
+                                message = new LoginResponseMessage(false, "用户名或密码不正确");
+                            }
+                            ctx.writeAndFlush(message);
+                        }
+                    });
                 }
             });
             ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
