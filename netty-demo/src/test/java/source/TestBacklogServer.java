b36d9ddc9a07e25a20f9b940e732742ecfa9027a
TonyHuang <1578501542@qq.com>
2021-11-06 15:28:40 +0800

    backlog

---
+++ netty-demo/src/test/java/source/TestBacklogServer.java
@@ -0,0 +1,30 @@
+package source;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.nio.charset.Charset;
+
+public class TestBacklogServer {
+    public static void main(String[] args) {
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup())
+                // 建立连接时候的参数，属于 ServerSocketChannel
+                .option(ChannelOption.SO_BACKLOG, 2) // 全队列满了
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new LoggingHandler());
+                    }
+                }).bind(8080);
+    }
+}
